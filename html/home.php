e2be7000c6d0b5478d21ee74090e6ceefdf43427
who am i <sacheeramadushan455@gmail.com>
2022-05-19 12:58:36 +0530

    Files

--- html/home.php
+++ html/home.php
@@ -2,9 +2,10 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport"  content ="width=device-width, initial-scale=1">
 	<title>My Class</title>
 	<link rel="stylesheet" type="text/css" href="../css/home.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 	<div class="main">
@@ -14,12 +15,91 @@
 			</div><!-- logo -->
 			<div class="nav">
 				<ul>
-					<a href="#"><li>Contact Us</li></a>
-					<a href="#"><li>About This Site</li></a>
-					<a href="#"><li>Log</li></a>
+					<li><a href="">Contact Us</a></li>
+					<li><a href="">About This Site</a></li>
+					<li><a href="">Log</a></li>
 				</ul>
 			</div><!-- nav -->
 		</div><!-- top -->
+
+		<div class="discription">
+			<h1>The Place Where The Students Meet Their Best Teachers.</h1>
+			<h2>Sri Lankan's #1 Class Adverticement Platform</h2>
+
+			<div class="button">
+				<button class="teacher">
+					I'm A Teacher
+				</button>
+				<button class="student">
+					I'm A Student
+				</button>
+			</div><!-- button -->
+			<!-- <div class="img">	
+				<img src="../src/main.png" width="100%">
+			</div>img -->
+			
+		</div><!-- discription -->
+
+		<div class="content">
+			<div class="student">
+				<div class="student-img">
+					<img src="../src/student.png" width="70%">
+				</div><!-- student-img -->
+				<div class="st-about">
+						
+					<h3>Benifits For Student</h3>
+					<p>
+						
+						Are you looking for a teacher? <br>	
+						So here is the best place to find your best teacher. Any type of Subject, Any type of Language, find your nearest physical or online class. Why are you still waiting for let's search for your best class?
+					</p>
+				</div><!-- st-abou -->
+			</div><!-- student -->
+
+			<div class="teacher">
+				<div class="teacher-img">
+					<img src="../src/teacher.png" width="80%">
+				</div><!-- teacher-img -->
+				<div class="teach-about">
+					<h3>Benifits For Teachers</h3>
+					<p>
+						Are you the best teacher? <br>	
+						So Why don't you tell it to the students who are looking for a best teacher like you? So you are in the right place. This is the best platform to say about you to everyone. Why <a href="	">	</a>re you still waiting? Let's post your first advertisement	
+					</p>
+				</div><!-- teach-about -->
+			</div><!-- teacher -->
+		</div><!-- content -->
+		<div class="popular">
+			<h1>-Popular Post-</h1>
+		</div>
+
+		<div class="dev">
+					<h1>Developers</h1>
+			<div class="dev-logo">
+				<img src="../src/logo.png" width="100%">
+			</div><!-- dev-logo -->
+			<div class="dev-discription">
+				<div class="dev-item">
+					<p>sdsgfdsg sgdfiugsifdg sjfjdfdshfg dggfddfdgffgdgif dfgfggfugidgfigigdf dggfgdgfgdugfug gdgfgdfgdif dfgdgfg</p>
+				</div><!-- dev-item -->
+				<div class="socialmedia">
+					<ul>
+						<li>
+							<a href="#" style="	color: darkblue;"	><i class="fa-brands fa-facebook fa-fw"></i></a>
+						</li>
+						<li>
+							<a href="#"style="	color: skyblue;"><i class="fa-brands fa-twitter fa-fw"></i></a>
+						</li>
+						<li>
+							<a href="#"style="	color: red;"><i class="fa-brands fa-youtube fa-fw"></i></a>
+						</li>
+
+					</ul>
+				</div><!-- socialmedia -->
+			</div><!-- dev-discription -->
+		</div><!-- dev -->
+
 	</div><!-- main -->
+
 </body>
 </html>
