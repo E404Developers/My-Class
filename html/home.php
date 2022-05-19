0035d6ba110a8a050a1cba596061e9bd77ec6351
who am i <sacheeramadushan455@gmail.com>
2022-05-19 12:46:18 +0530

    Files

---
+++ html/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Class</title>
+	<link rel="stylesheet" type="text/css" href="../css/home.css">
+</head>
+<body>
+	<div class="main">
+		<div class="top">
+			<div class="logo">
+				<h1>My Class</h1>
+			</div><!-- logo -->
+			<div class="nav">
+				<ul>
+					<a href="#"><li>Contact Us</li></a>
+					<a href="#"><li>About This Site</li></a>
+					<a href="#"><li>Log</li></a>
+				</ul>
+			</div><!-- nav -->
+		</div><!-- top -->
+	</div><!-- main -->
+</body>
+</html>
