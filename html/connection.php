92e8886bb2932bb6d4d858b1961dd1f7ddcd5410
who am i <sacheeramadushan455@gmail.com>
2022-05-20 11:43:51 +0530

    Update connection.php

--- html/connection.php
+++ html/connection.php
@@ -1,6 +1,6 @@
 <?php 
 	date_default_timezone_set("Asia/Colombo");
-	$con = mysqli_connect("localhost","root","","dcssite");
+	$con = mysqli_connect("localhost","root","","myclass");
 
 	if(!($con)){
 		die("Server Error !");
